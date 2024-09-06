e6cf6a95d3537bc30b8bfaa04951487c3c593f79
KevediBeb <kevin.torneus@elev.ga.ntig.se>
2024-09-06 09:36:46 +0200

    skibidi!

---
+++ program.py
@@ -0,0 +1,35 @@
+import random
+
+#win condition
+winCon = 30
+
+#player cash
+p = 0
+pDead = False
+
+#computer cash
+c = 0
+cDead = False
+
+dice = 0
+dice = random.randint(1,6)
+print(dice)
+
+#ROUND TEST
+
+#Spelare 
+roundtype = 0
+
+
+
+dice = 0
+dice = random.randint(1,6)
+
+
+
+#while(pDead == False or cDead == False):    
+    
+
+
+
+
